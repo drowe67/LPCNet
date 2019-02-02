42a256bed4f88669134e39357a4b2e743d9460d4
David <david@rowetel.com>
2019-02-02 11:14:34 +1030

    scripts to plot loss of training while in progress

---
+++ src/plot_train.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+loss = np.loadtxt(sys.argv[1])
+delta_loss = (loss[1:]-loss[:-1])/loss[1:]
+
+plt.figure(1)
+plt.plot(loss)
+plt.title('loss')
+plt.show(block=False)
+plt.figure(2)
+plt.plot(delta_loss)
+plt.title('% delta loss')
+#plt.ylim([-1E-2,1E-2])
+plt.show()

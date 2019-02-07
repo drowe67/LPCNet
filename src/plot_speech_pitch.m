b08156e7ba187844dde232e934c84c970f8b7ec8
David <david@rowetel.com>
2019-02-08 10:24:37 +1030

    added mode to plot pitch and pitch gain (voicing est)

--- src/plot_speech_pitch.m
+++ src/plot_speech_pitch.m
@@ -1,8 +1,14 @@
 % plot_speech_pitch.m
 % David Rowe Jan 2019
 %
-% Octave script to speech and the pitch F0 in Hz from a .f32 feature
-% file against time. Runs from the command line and outputs a PNG.
+% Octave script to plot speech and the pitch F0 in Hz from a .f32 feature
+% file against time. Runs from the shell command line and outputs a PNG.
+%
+% usage1 - plot pitch est in feastures1 and features2.f32 together:
+%   $ octave --no-gui -p src -qf src/plot_speech_pitch.m speech.s16 features1.f32 features2.f32 plot.png 400
+%
+% usage2 - ploty pitch est and voicing from features1.f32 together:
+%   $ octave --no-gui -p src -qf src/plot_speech_pitch.m speech.s16 features1.f32 - plot.png 400
 
 nb_lpcnet_features=55;
 nb_lpcnet_bands=18;
@@ -15,7 +21,7 @@ graphics_toolkit ("gnuplot")
 
 arg_list = argv ();
 if nargin == 0
-  printf("\nusage: %s rawSpeechFile featureFile featureFile_c2 PNGname f0AxisMax\n\n", program_name());
+  printf("\nusage: %s rawSpeechFile featureFile [featureFile_c2 | -] PNGname f0AxisMax\n\n", program_name());
   exit(0);
 end
 
@@ -27,9 +33,17 @@ feat=load_f32(fn_feat, nb_lpcnet_features);
 pitch_index_lpcnet = 100*feat(:,2*nb_lpcnet_bands+1) + 200;
 f0 = 2*Fs ./ pitch_index_lpcnet;
 
-feat_c2=load_f32(fn_feat_c2, nb_lpcnet_features);
-pitch_index_c2 = 100*feat_c2(:,2*nb_lpcnet_bands+1) + 200;
-f0_c2 = 2*Fs ./ pitch_index_c2;
+if !strcmp(fn_feat_c2,"-")
+  % usage1 - two pitch estimators
+  usage = 1;
+  feat_c2=load_f32(fn_feat_c2, nb_lpcnet_features);
+  pitch_index_c2 = 100*feat_c2(:,2*nb_lpcnet_bands+1) + 200;
+  f0_c2 = 2*Fs ./ pitch_index_c2;
+else
+  % usage2 - pitch plus "gain" (voicing)
+  usage = 2;
+  gain = feat(:,2*nb_lpcnet_bands+2);
+end
 
 fs=fopen(fn_raw,"rb");
 s = fread(fs,Inf,"short");
@@ -48,15 +62,23 @@ axis([st_sec en_sec -mx mx])
 subplot(212,"position",[0.1 0.05 0.8 0.7]);
 st = Fsp*st_sec; en = Fsp*en_sec;
 t = st_sec:1/Fsp:en_sec-1/Fsp;
-plot(t,f0(st+1:en),'b;F0 Hz;');
-hold on;
-plot(t,f0_c2(st+1:en),'g;F0 Hz CODEC 2;');
-hold off;
-ylabel('F0 Hz')
 
 % adjust scale to make plot clearer
 mx = str2num(arg_list{5})
-axis([st_sec en_sec 50 mx])
+
+if usage == 1
+  plot(t,f0(st+1:en),'b;F0 Hz;');
+  hold on;
+  plot(t,f0_c2(st+1:en),'g;F0 Hz CODEC 2;');
+  hold off;
+  ylabel('F0 Hz')
+  axis([st_sec en_sec 50 mx])
+else
+  ax = plotyy(t,f0(st+1:en),t,gain(st+1:en));
+  ylabel (ax(1), "F0 HZ");
+  ylabel (ax(2), "GAIN");
+  axis(ax(1), [st_sec en_sec 50 mx])
+end
 
 str=sprintf("-S%d,700",floor(1200*length(s)/(2*Fs)))
 print(arg_list{4},'-dpng',str)

84435bd684c88910303f9b4bcb68d66f8420fea1
David <david@rowetel.com>
2019-02-08 10:23:56 +1030

    added --lite option for quick comparisons, uses cmd line args inbstead of env variables

--- process.sh
+++ process.sh
@@ -2,32 +2,59 @@
 # process.sh
 # David Rowe Jan 2019
 #
-# 1. Process an input set of wave files using LPCNet under a variety of conditions.
-# 2. Name output files to make them convenient to listen to in a file manager.
-# 3. Generate a HTML table of samples for convenient replay on the web.
-# 4. Generate a bunch of other HTML files and PNGs.
-#
-# usage: OUTPATH=!/your/output/path ./process.sh
-# usage: OUTPATH=!/your/output/path OUTPATHB=!/your/output/pathB ./process.sh
+: '
+  1. Process an input set of wave files using LPCNet under a variety of conditions.
+  2. Name output files to make them convenient to listen to in a file manager.
+  3. Generate a HTML table of samples for convenient replay on the web.
+  4. Generate a bunch of other HTML files and PNGs.
+
+  usage: ./process.sh [--lite] OutPath
+         ./process.sh ~/tmp/lpcnet_out
+  
+  To compare generate new samples OutPathA, and compare with those from a previous 
+  run of this script in OutPathB:
+
+        ./process.sh OutPathA OutPathB
+
+  --lite generates a much smaller page with just the basic LPCNet model case
+'
+
+# command line arguments
+
+if [ $# -lt 1 ]; then
+    echo "usage: ./process2.sh [--lite] /output/path/1 [/output/path/2]"
+    echo "       $ ./process.sh ~/tmp/lpcnet_outA"
+    exit 1
+fi
+
+lite=0
+for i in "$@"
+do
+case $i in
+    --lite)
+        lite=1
+        shift
+    ;;
+esac
+done
+
+OUTPATH=$1
+if [ $# -eq 2 ]; then
+    OUTPATHB=$2
+fi
 
 # set these paths to suit your system
 CODEC2_PATH=$HOME/codec2-dev/build_linux/src
 WAVIN_PATH=$HOME/Desktop/deep/quant
 
-# OUTPATH=$HOME/tmp/lpcnet_out
-if [ -z "${OUTPATH}" ]; then
-    echo "please set OUTPATH"
-    exit 1
-fi
-
 WAVOUT_PATH=$OUTPATH/wav
 PATH=$PATH:$CODEC2_PATH
 STATS=$OUTPATH/stats.txt
-HTML=$OUTPATH/results.html
+HTML=$OUTPATH/index.html
 PNG_PATH=$OUTPATH/png
 F32_PATH=$OUTPATH/f32
 SV_PATH=$OUTPATH/sv
-WAV_FILES="birch glue oak separately wanted wia"
+WAV_FILES="all birch canadian glue oak separately wanted wia"
 
 # check we can find wave files
 for f in $WAV_INFILES
@@ -52,6 +79,8 @@ mkdir -p $WAVOUT_PATH
 mkdir -p $PNG_PATH
 rm -f $STATS
 
+vq=pred_v2_stage
+
 # cp in originals
 for f in $WAV_FILES
 do
@@ -65,63 +94,67 @@ do
     ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_1_uq'.wav
 done
 
-# 3dB uniform quantiser, 10ms updates
-for f in $WAV_FILES
-do
-    label=$(printf "3dB %-10s" "$f")
-    sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
-    ./quant_feat -l "$label" -d 1 --uniform 3 2>>$STATS | ./test_lpcnet - - | \
-    sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_2_3dB'.wav
-                                                           
-done
+if [ $lite -eq 0 ]; then 
 
-# decimate features to 20ms updates, then linearly interpolate back up to 10ms updates
-for f in $WAV_FILES
-do
-    sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
-        ./quant_feat -d 2 | ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_3_20ms'.wav 
-        
-done
+     # 3dB uniform quantiser, 10ms updates
+    for f in $WAV_FILES
+    do
+        label=$(printf "3dB %-10s" "$f")
+        sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
+        ./quant_feat -l "$label" -d 1 --uniform 3 2>>$STATS | ./test_lpcnet - - | \
+        sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_2_3dB'.wav
 
-# 33 bit 3 stage VQ searched with mbest algorithm, 20ms updates
-for f in $WAV_FILES
-do
-    label=$(printf "33bit_20ms %-10s" "$f")
-    sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
-    ./quant_feat -l "$label" -d 2 --mbest 5 -q pred2_stage1.f32,pred2_stage2.f32,pred2_stage3.f32 -s $SV_PATH/$f'_4_33bit_20ms'.txt  2>>$STATS | \
-    ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_4_33bit_20ms'.wav
-done
+    done
 
-# 33 bit 3 stage VQ searched with mbest algorithm, 30ms updates
-for f in $WAV_FILES
-do
-    label=$(printf "33bit_30ms %-10s" "$f")
-    sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
-        ./quant_feat -l "$label" -d 3 --mbest 5 -q pred2_stage1.f32,pred2_stage2.f32,pred2_stage3.f32 -s $SV_PATH/$f'_5_33bit_30ms'.txt 2>>$STATS | \
-        ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_5_33bit_30ms'.wav
-done
+    # decimate features to 20ms updates, then linearly interpolate back up to 10ms updates
+    for f in $WAV_FILES
+    do
+        sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
+            ./quant_feat -d 2 | ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_3_20ms'.wav 
 
-# 44 bit 4 stage VQ searched with mbest algorithm, 30ms updates
-for f in $WAV_FILES
-do
-    label=$(printf "44bit_30ms %-10s" "$f")
-    sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
-    ./quant_feat -l "$label" -d 3 --mbest 5 -q pred2_stage1.f32,pred2_stage2.f32,pred2_stage3.f32,pred2_stage4.f32 -s $SV_PATH/$f'_6_44bit_30ms'.txt 2>>$STATS | \
-    ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_6_44bit_30ms'.wav
-done
+    done
 
-# Codec 2 and simulated SSB (10dB SNR, AWGN channel, 10Hz freq offset) reference samples
-for f in $WAV_FILES
-do
-    sox $WAVIN_PATH/$f.wav -t raw -r 8000 - | c2enc 2400 - - | c2dec 2400 - - | \
-    sox -r 8000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_7_c2_2400'.wav
-done
-for f in $WAV_FILES
-do
-    sox -r 16000 -c 1 $WAVIN_PATH/$f.wav -r 8000 -t raw - | \
-        cohpsk_ch - - -35 --Fs 8000 -f 10 --ssbfilt 1 |  \
-    sox -r 8000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_8_ssb_10dB'.wav
-done
+    # 33 bit 3 stage VQ searched with mbest algorithm, 20ms updates
+    for f in $WAV_FILES
+    do
+        label=$(printf "33bit_20ms %-10s" "$f")
+        sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
+        ./quant_feat -l "$label" -d 2 -w --mbest 5 -q $vq'1.f32',$vq'2.f32',$vq'3.f32' -s $SV_PATH/$f'_4_33bit_20ms'.txt  2>>$STATS | \
+        ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_4_33bit_20ms'.wav
+    done
+
+    # 33 bit 3 stage VQ searched with mbest algorithm, 30ms updates
+    for f in $WAV_FILES
+    do
+        label=$(printf "33bit_30ms %-10s" "$f")
+        sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
+            ./quant_feat -l "$label" -d 3 -w --mbest 5 -q $vq'1.f32',$vq'2.f32',$vq'3.f32' -s $SV_PATH/$f'_5_33bit_30ms'.txt 2>>$STATS | \
+            ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_5_33bit_30ms'.wav
+    done
+
+    # 44 bit 4 stage VQ searched with mbest algorithm, 30ms updates
+    for f in $WAV_FILES
+    do
+        label=$(printf "44bit_30ms %-10s" "$f")
+        sox $WAVIN_PATH/$f.wav -t raw - | ./dump_data --test --c2pitch - - | \
+        ./quant_feat -l "$label" -d 3 -w --mbest 5 -q $vq'1.f32',$vq'2.f32',$vq'3.f32',$vq'4.f32' -s $SV_PATH/$f'_6_44bit_30ms'.txt 2>>$STATS | \
+        ./test_lpcnet - - | sox -r 16000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_6_44bit_30ms'.wav
+    done
+
+    # Codec 2 and simulated SSB (10dB SNR, AWGN channel, 10Hz freq offset) reference samples
+    for f in $WAV_FILES
+    do
+        sox $WAVIN_PATH/$f.wav -t raw -r 8000 - | c2enc 2400 - - | c2dec 2400 - - | \
+        sox -r 8000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_7_c2_2400'.wav
+    done
+    for f in $WAV_FILES
+    do
+        sox -r 16000 -c 1 $WAVIN_PATH/$f.wav -r 8000 -t raw - | \
+            cohpsk_ch - - -35 --Fs 8000 -f 10 --ssbfilt 1 |  \
+        sox -r 8000 -t .s16 -c 1 - $WAVOUT_PATH/$f'_8_ssb_10dB'.wav
+    done
+
+fi # ... if [ $lite -eq 0 ] ...
 
 #
 # Create a HTML table of samples ----------------------------------------------------
@@ -132,56 +165,52 @@ cat << EOF > $HTML
 <html>
 <head>
   <title>LPCNet Samples</title>
-  <style type="text/css">
-  <body>table {
-    table-layout: fixed ;
-    width: 100% ; 
-  }
-  td {
-    width: 10% ;
-  } 
-  </style>
+
 </head>
 <body>
-
-<table>
-<col width="10%">
-<col width="70%">
-<caption>Glossary</caption>
-<tr><th align="left">Term</th><th align="left">Description</th></tr>
-<tr><td>Orig</td><td>Original source input speech</td></tr>
-<tr><td>UQ</td><td>Baseline LPCNet synthesis using unquantised features</td></tr>
-<tr><td>3dB</td><td>Cesptral features uniform quantiser with 3dB steps</td></tr>
-<tr>
-  <td>20ms</td>
-  <td>Cesptral features decimated to 20ms frame rate, linear interpolation back to 10ms</td>
-</tr>
-<tr>
-  <td>33bit_20ms</td>
-  <td>3 stage VQ of prediction error, 11 bits/stage, at 20ms frame rate</td>
-</tr>
-<tr>
-  <td>33bit_30ms</td>
-  <td>Same 33 bit VQ, but decimated down to 30ms rate</td>
-</tr>
-<tr>
-  <td>44bit_40ms</td>
-  <td>4 stage VQ, at 30ms update rate</td>
-</tr>
-<tr>
-  <td>c2_2400</td>
-  <td>Codec 2 at 2400 bits/s</td>
-</tr>
-<tr>
-  <td>ssb_10dB</td>
-  <td>Analog Single Sideband at a Signal/Noise ratio (in 3000 Hz) of 10dB.  Band 
-      limited to 300-2600Hz.  At similar SNR with an appropriate modem we can send 
-      error free 2000 bit/s coded speech.
-  </td>
-</tr>
-</table>
-<p>
 EOF
+
+if [ $lite -eq 0 ]; then
+    cat << EOF > $HTML
+    <table>
+    <col width="10%">
+    <col width="70%">
+    <caption>Glossary</caption>
+    <tr><th align="left">Term</th><th align="left">Description</th></tr>
+    <tr><td>Orig</td><td>Original source input speech</td></tr>
+    <tr><td>UQ</td><td>Baseline LPCNet synthesis using unquantised features</td></tr>
+    <tr><td>3dB</td><td>Cesptral features uniform quantiser with 3dB steps</td></tr>
+    <tr>
+      <td>20ms</td>
+      <td>Cesptral features decimated to 20ms frame rate, linear interpolation back to 10ms</td>
+    </tr>
+    <tr>
+      <td>33bit_20ms</td>
+      <td>3 stage VQ of prediction error, 11 bits/stage, at 20ms frame rate</td>
+    </tr>
+    <tr>
+      <td>33bit_30ms</td>
+      <td>Same 33 bit VQ, but decimated down to 30ms rate</td>
+    </tr>
+    <tr>
+      <td>44bit_40ms</td>
+      <td>4 stage VQ, at 30ms update rate</td>
+    </tr>
+    <tr>
+      <td>c2_2400</td>
+      <td>Codec 2 at 2400 bits/s</td>
+    </tr>
+    <tr>
+      <td>ssb_10dB</td>
+      <td>Analog Single Sideband at a Signal/Noise ratio (in 3000 Hz) of 10dB.  Band 
+          limited to 300-2600Hz.  At similar SNR with an appropriate modem we can send 
+          error free 2000 bit/s coded speech.
+      </td>
+    </tr>
+    </table>
+    <p>
+EOF
+fi
 
 function heading_row {
     w=$(echo $WAV_FILES | cut -d ' ' -f 1)
@@ -212,7 +241,7 @@ do
             # no comparison
             printf "  <td align="center"><a href=\"wav/%s\">play</a></td>\n" $b >> $HTML
         else
-            # compare with another process.sh run            
+            # compare with another process.sh run
             printf "  <td align="center"><a href=\"wav/%s\">play</a> (<a href=\"%s\">playB</a>) </td>\n" $b $OUTPATHB/wav/$b >> $HTML
         fi
     done
@@ -278,45 +307,45 @@ function table_of_values {
     printf "</table><p>\n" >> $HTML
 }
 
-table_of_values "var" "Variance"
-table_of_values "sd"  "Standard Deviation"
-table_of_values "outliers" "Outliers"
+if [ $lite -eq 0 ]; then
+    table_of_values "var" "Variance"
+    table_of_values "sd"  "Standard Deviation"
+    table_of_values "outliers" "Outliers"
 
-#
-# Table of Speech/Pitch countours ----------------------------------------------
-#
+    #
+    # Table of Speech/Pitch countours ----------------------------------------------
+    #
 
-printf "<table>\n" >> $HTML
-printf "<caption>Pitch Countours</caption>\n" >> $HTML
+    printf "<table>\n" >> $HTML
+    printf "<caption>Pitch Countours</caption>\n" >> $HTML
 
-# heading row
-printf "<tr>\n" >> $HTML
-for f in $WAV_FILES
-do
-    printf "  <th>%s</th>\n" $f >> $HTML
-done
-printf "</tr>\n" >> $HTML
+    # heading row
+    printf "<tr>\n" >> $HTML
+    for f in $WAV_FILES
+    do
+        printf "  <th>%s</th>\n" $f >> $HTML
+    done
+    printf "</tr>\n" >> $HTML
 
-# manually set pitch axis to make plots easier to read
-mx=(200 400 400 200 400 200)
-count=0
+    # manually set pitch axis to make plots easier to read.  TODO this breaks when we add new samples, need an associative array
+    mx=(400 200 200 400 400 200 400 200)
+    count=0
 
-# row of links to PNGs
-printf "<tr>\n" >> $HTML
-for f in $WAV_FILES
-do
-    sox $WAVIN_PATH/$f.wav -t raw $F32_PATH/$f.raw
-    ./dump_data --test $F32_PATH/$f.raw $F32_PATH/$f.f32
-    ./dump_data --test --c2pitch $F32_PATH/$f.raw $F32_PATH/$f'_c2'.f32
-    octave --no-gui -p src -qf src/plot_speech_pitch.m $F32_PATH/$f.raw  $F32_PATH/$f.f32 $F32_PATH/$f'_c2'.f32 $PNG_PATH/$f'_pitch.png' ${mx[count]}
-    count=$(( $count + 1 ))
-    b=$f'_pitch.png'
-    printf "  <td align="center"><a href=\"png/%s\"><img width=100 height=100 src=\"png/%s\" /></a></td>\n" $b $b >> $HTML
-done
-printf "</tr>\n" >> $HTML
-printf "</table><p>\n" >> $HTML
+    # row of links to PNGs
+    printf "<tr>\n" >> $HTML
+    for f in $WAV_FILES
+    do
+        sox $WAVIN_PATH/$f.wav -t raw $F32_PATH/$f.raw
+        ./dump_data --test --c2pitch $F32_PATH/$f.raw $F32_PATH/$f'_c2'.f32
+        octave --no-gui -p src -qf src/plot_speech_pitch.m $F32_PATH/$f.raw  $F32_PATH/$f'_c2'.f32 - $PNG_PATH/$f'_pitch.png' ${mx[count]}
+        count=$(( $count + 1 ))
+        b=$f'_pitch.png'
+        printf "  <td align="center"><a href=\"png/%s\"><img width=100 height=100 src=\"png/%s\" /></a></td>\n" $b $b >> $HTML
+    done
+    printf "</tr>\n" >> $HTML
+    printf "</table><p>\n" >> $HTML
 
-table_of_values "quant" "Quantiser Error Countours"
+    table_of_values "quant" "Quantiser Error Countours"
+fi
 
 printf "</body>\n</html>\n" >> $HTML
-

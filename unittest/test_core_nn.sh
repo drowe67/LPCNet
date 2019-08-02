7cc35231ab0a0a8575df983891fe127627e522ee
David <david@rowetel.com>
2019-08-02 16:16:38 +0930

    first pass a ctests and Travis support

--- unittest/test_core_nn.sh
+++ unittest/test_core_nn.sh
@@ -4,15 +4,15 @@
 # Some tests for core NN, e.g. generation of test data using
 # dump_data, and unquantised synthesis using and test_lpcnet
 
-TRAIN_SRC=all_speech
-TRAIN_OUT_PCM=$(mktemp).pcm
-TRAIN_OUT_F32=$(mktemp).f32
-TRAIN_TARG_PCM=all_speech.pcm
-TRAIN_TARG_F32=all_speech.f32
 
 # test generation of training data (doesn't really test training as that takes hours)
-
+# TODO: This test not working yet
 if [ ! -z $TRAIN_TEST ]; then
+    TRAIN_SRC=all_speech
+    TRAIN_OUT_PCM=$(mktemp).pcm
+    TRAIN_OUT_F32=$(mktemp).f32
+    TRAIN_TARG_PCM=all_speech.pcm
+    TRAIN_TARG_F32=all_speech.f32
     ../build_linux/src/dump_data --train --c2pitch -z 0 -n 1E6 ~/Downloads/$TRAIN_SRC.sw $TRAIN_OUT_F32 $TRAIN_OUT_PCM
     diff $TRAIN_OUT_F32 $TRAIN_TARG_F32 || { echo "ERROR in train .f32 output! Exiting..."; exit 1; }
     echo "train .f32 OK"
@@ -26,7 +26,7 @@ if [ ! -z $SYNTH ]; then
     ../build_linux/src/dump_data --test --c2pitch ../wav/birch.wav birch.f32
     diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "synth .f32 OK"
-    ../build_linux/src/test_lpcnet -n lpcnet_190215.f32 -l birch_states.f32 birch.f32 birch_out.raw
+    ../build_linux/src/test_lpcnet -l birch_states.f32 birch.f32 birch_out.raw
     octave -p ../src --no-gui <<< "ret=compare_states('birch_states_targ.f32', 'birch_states.f32'); quit(ret)"
     if [ ! $? -eq 0 ]; then { echo "ERROR in synth states Octave output! Exiting..."; exit 1; } fi
     echo "synth states Octave OK"			    
@@ -42,7 +42,7 @@ if [ ! -z $SYNTH_20h ]; then
     ../build_linux/src/dump_data --test --c2pitch ../wav/birch.wav birch.f32
     diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "synth .f32 OK"
-    ../build_linux/src/test_lpcnet -l birch_states.f32 birch.f32 birch_out.raw
+    ../build_linux/src/test_lpcnet -n lpcnet_20h.f32 -l birch_states.f32 birch.f32 birch_out.raw
     octave -p ../src --no-gui <<< "ret=compare_states('birch_20h_states_targ.f32', 'birch_states.f32'); quit(ret)"
     if [ ! $? -eq 0 ]; then { echo "ERROR in synth states Octave output! Exiting..."; exit 1; } fi
     echo "synth states Octave OK"			    

85df2d36e77f86dcbc4613c590b6e366ba28a8a1
David <david@rowetel.com>
2019-08-02 15:36:35 +0930

    synth and synth_20h tests

--- unittest/test_core_nn.sh
+++ unittest/test_core_nn.sh
@@ -10,16 +10,6 @@ TRAIN_OUT_F32=$(mktemp).f32
 TRAIN_TARG_PCM=all_speech.pcm
 TRAIN_TARG_F32=all_speech.f32
 
-SYNTH_SRC=birch.wav
-SYNTH_OUT_F32=birch.f32
-SYNTH_OUT_RAW=birch_out.raw
-SYNTH_STATES_OUT=birch_states_out.f32
-SYNTH_TARG_F32=birch_targ.f32
-SYNTH_TARG_RAW=birch_out_targ.raw
-SYNTH_STATES_TARG=birch_states_targ.f32
-
-SYNTH_TEST=1
-
 # test generation of training data (doesn't really test training as that takes hours)
 
 if [ ! -z $TRAIN_TEST ]; then
@@ -30,9 +20,9 @@ if [ ! -z $TRAIN_TEST ]; then
     echo "train .pcm OK"
 fi
 
-# test basic synthesis with build in network
+# Basic synthesis with compiled-in in network
 
-if [ ! -z $SYNTH_TEST ]; then
+if [ ! -z $SYNTH ]; then
     ../build_linux/src/dump_data --test --c2pitch ../wav/birch.wav birch.f32
     diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "synth .f32 OK"
@@ -43,6 +33,22 @@ if [ ! -z $SYNTH_TEST ]; then
     diff birch_states_targ.f32 birch_states.f32 || { echo "ERROR in synth states output! Exiting ..."; exit 1; }
     echo "synth states OK"
     diff birch_out_targ.raw birch_out.raw || { echo "ERROR in synth .raw output! Exiting..."; exit 1; }
+    echo "synth .raw OK"
+fi
+
+# Synthesis with the 20h network, loaded up at run time
+    
+if [ ! -z $SYNTH_20h ]; then
+    ../build_linux/src/dump_data --test --c2pitch ../wav/birch.wav birch.f32
+    diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
+    echo "synth .f32 OK"
+    ../build_linux/src/test_lpcnet -l birch_states.f32 birch.f32 birch_out.raw
+    octave -p ../src --no-gui <<< "ret=compare_states('birch_20h_states_targ.f32', 'birch_states.f32'); quit(ret)"
+    if [ ! $? -eq 0 ]; then { echo "ERROR in synth states Octave output! Exiting..."; exit 1; } fi
+    echo "synth states Octave OK"			    
+    diff birch_20h_states_targ.f32 birch_states.f32 || { echo "ERROR in synth states output! Exiting ..."; exit 1; }
+    echo "synth states OK"
+    diff birch_20h_targ.raw birch_out.raw || { echo "ERROR in synth .raw output! Exiting..."; exit 1; }
     echo "synth .raw OK"
 fi
 

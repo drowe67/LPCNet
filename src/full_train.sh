02ecf2ac3085f8a1aeaec5655b73b05c84840dd2
David <david@rowetel.com>
2019-09-24 19:00:44 +0930

    190924a, using original LPCNet voicing est (pitch gain).  I have accidentally written over the 190922a network with c2voicing, so will run again

--- src/full_train.sh
+++ src/full_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 # full_train.sh
-# Script to train using full database, maerial developed to give reasonable quality
+# Script to train using full database, material developed to give reasonable quality
 # to test sample database.
 #
 # usage:
@@ -10,14 +10,12 @@
 SRC1=david_16kHz.wav             # 122s
 SRC2=vk5apr_recording_21_may.wav # 64s
 SRC3=all_speechcat.sw            # 185 minutes, wide range of speakers
-SRC4=wianews-2019-01-20.s16      # 62 minutes
-SRC5=soldersmoke202.sw           # 76 minutes, lot of material with target voices
 SRC=train_src
 
-DATE=190922a
+DATE=190924a
 
 synth() {
-  ./src/dump_data --test --c2pitch --c2voicing ~/Downloads/$1.sw $1.f32
+  ./src/dump_data --test --c2pitch ~/Downloads/$1.sw $1.f32
   ./src/test_lpcnet $1.f32 "$2".raw
 }
 
@@ -32,16 +30,18 @@ train() {
 	-t sw -r 16000 -c 1 ~/Downloads/$SRC3 \
 	-t sw $SRC.sw
     ls -l $SRC.sw
-    ./src/dump_data --train --c2pitch --c2voicing -z 1 $SRC.sw $SRC.f32 $SRC.pcm
+    ./src/dump_data --train --c2pitch -z 1 $SRC.sw $SRC.f32 $SRC.pcm
     ../src/train_lpcnet.py $SRC.f32 $SRC.pcm lpcnet_$DATE
     ../src/dump_lpcnet.py lpcnet_"$DATE"_10.h5
     cp nnet_data.c src
     make test_lpcnet
 }
 
-#train
-#synth c01_01 $DATE'_f'
-#synth mk61_01 $DATE'_m'
-#synth cq_16kHz $DATE'_cq_16kHz'
-#synth peter $DATE'_peter'
-synth wia $DATE'_wia'
+train
+synth c01_01 $DATE'_f'
+synth mk61_01 $DATE'_m'
+synth cq_16kHz $DATE'_cq_16kHz'
+synth peter $DATE'_peter'
+# source in a different dir
+./src/dump_data --test --c2pitch ~/LPCNet/wav/all.wav all.f32
+./src/test_lpcnet all.f32 $DATE'_all'.raw

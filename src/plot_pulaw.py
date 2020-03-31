e46fb894647aa1b9dc14d07b39b74cc6dacb19e6
David <david@rowetel.com>
2020-04-01 07:54:46 +1030

    experiments with no/first order/LPC/Q in and out of loop  predictors

--- src/plot_pulaw.py
+++ src/plot_pulaw.py
@@ -1,4 +1,5 @@
-# plot packed ulaw samples
+#!/usr/bin/python3
+# Utility to inspect packed ulaw samples before training from sw2packedulaw.c
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,23 +9,46 @@ import ulaw
 data = np.fromfile(sys.argv[1], dtype='uint8')
 nb_samples = 8000
 
-data = data[:nb_samples*4]
+#data = data[:nb_samples*20]
 
 sig = np.array(data[0::4], dtype='float')
 pred = np.array(data[1::4], dtype='float')
 in_exc = np.array(data[2::4], dtype='float')
 out_exc = np.array(data[3::4], dtype='float')
+   
+print("exc var: %4.3e" % (np.var(ulaw.ulaw2lin(in_exc))))
 
+"""
 s_in = 32000*np.sin(np.arange(0,8000)*np.pi*2/100)
 s_out = ulaw.ulaw2lin(ulaw.lin2ulaw(s_in))
 plt.figure(1)
 plt.plot(s_in)
 plt.plot(s_out)
 plt.show(block=False)
+"""
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(ulaw.ulaw2lin(sig), label='sig')
+plt.ylim((-30000,30000))
+plt.legend()
+plt.subplot(212)
+plt.plot(ulaw.ulaw2lin(pred), label='pred')
+plt.ylim((-30000,30000))
+plt.legend()
+plt.show(block=False)
 
 plt.figure(2)
-plt.plot(ulaw.ulaw2lin(sig))
-plt.plot(ulaw.ulaw2lin(pred))
-plt.plot(ulaw.ulaw2lin(in_exc))
-plt.plot(ulaw.ulaw2lin(out_exc))
+plt.subplot(211)
+plt.plot(ulaw.ulaw2lin(in_exc), label='in_exc')
+if len(sys.argv) == 3:
+    data2 = np.fromfile(sys.argv[2], dtype='uint8')
+    in_exc2 = np.array(data2[2::4], dtype='float')
+    plt.plot(ulaw.ulaw2lin(in_exc2), label='in_exc2')
+plt.ylim((-30000,30000))
+plt.legend()
+plt.subplot(212)
+plt.plot(ulaw.ulaw2lin(out_exc), label='out_exc')
+plt.ylim((-30000,30000))
+plt.legend()
 plt.show()
--- src/sw2packedulaw.c
+++ src/sw2packedulaw.c
@@ -2,7 +2,25 @@
   sw2packedulaw.c
 
   Convert signed word samples to packed ulaw samples to drive LPCNet
-  training, cut/paste from dump_data.c
+  training, this code is a cut/paste from dump_data.c witha few other
+  options.
+
+  Generate some training data:
+
+    ~/codec2/build_linux/src/c2sim ~/Downloads/all_8k.sw --ten_ms_centre all_8k_10ms.sw --rateKWov all_8k.f32 --lpc 10
+
+  Then:
+
+  1. No prediction (WaveRNN I guess):
+    $ ./src/sw2packedulaw --frame_size 80 all_8k_10ms.sw all_8k_none.pulaw
+  2. First order predictor:
+    $ ./src/sw2packedulaw --frame_size 80 --first all_8k_10ms.sw all_8k_first.pulaw
+  3. LPC with ulaw Q in the loop and noise injection (standard LPCNet design):
+    $ ./src/sw2packedulaw --frame_size 80 --lpc all_8k.f32 all_8k_10ms.sw all_8k.pulaw
+  4. LPC with no Q in the loop or noise injection (linear):
+    $ ./src/sw2packedulaw --frame_size 80 --linear --lpc all_8k.f32 all_8k_10ms.sw all_8k_linear.pulaw
+
+  See plot_pulaw.py to inspect output .pulaw files
 */
 
 #include <stdlib.h>
@@ -14,6 +32,10 @@
 #include "arch.h"
 #include "celt_lpc.h"
 #include <assert.h>
+#include <getopt.h>
+
+#define NB_FEATURES 55
+#define CODEC2_LPC_ORDER 10
 
 typedef struct {
   float lpc[LPC_ORDER];
@@ -21,10 +43,10 @@ typedef struct {
   int exc_mem;
 } DenoiseState;
 
-void write_audio(DenoiseState *st, const short *pcm, float noise_std, FILE *file) {
+void write_audio(DenoiseState *st, const short *pcm, float noise_std, FILE *file, int frame_size) {
   int i;
-  unsigned char data[4*FRAME_SIZE];
-  for (i=0;i<FRAME_SIZE;i++) {
+  unsigned char data[4*frame_size];
+  for (i=0;i<frame_size;i++) {
     int noise;
     float p=0;
     float e;
@@ -48,24 +70,124 @@ void write_audio(DenoiseState *st, const short *pcm, float noise_std, FILE *file
     st->sig_mem[0] = p + ulaw2lin(e);
     st->exc_mem = e;
   }
-  fwrite(data, 4*FRAME_SIZE, 1, file);
+  fwrite(data, 4*frame_size, 1, file);
+}
+
+/* takes ulaw out of predictor path, and no noise injection */
+void write_audio_linear(DenoiseState *st, const short *pcm, FILE *file, int frame_size) {
+  int i;
+  unsigned char data[4*frame_size];
+  for (i=0;i<frame_size;i++) {
+    float p=0;
+    float e;
+    int j;
+    for (j=0;j<LPC_ORDER;j++) p -= st->lpc[j]*st->sig_mem[j];
+    e = pcm[i] - p;
+    //fprintf(stderr,"pcm: %d p: %f e: %f\n", pcm[i], p, e);
+    /* Signal. */
+    data[4*i] = lin2ulaw(st->sig_mem[0]);
+    /* Prediction. */
+    data[4*i+1] = lin2ulaw(p);
+    /* Excitation in. */
+    data[4*i+2] = st->exc_mem;
+    /* Excitation out. */
+    data[4*i+3] = lin2ulaw(e);
+
+    RNN_MOVE(&st->sig_mem[1], &st->sig_mem[0], LPC_ORDER-1);
+    st->sig_mem[0] = pcm[i];
+    st->exc_mem = lin2ulaw(e);
+  }
+  fwrite(data, 4*frame_size, 1, file);
 }
 
 int main(int argc, char *argv[]) {
-    FILE *fsw = fopen(argv[1],"rb");
-    assert(fsw != NULL);
-    FILE *fpackedpcm = fopen(argv[2],"wb");
-    assert(fpackedpcm != NULL);
-
-    /* running with LPCs, so WaveRNN rather than LPCNet */
+    FILE *ffeature = NULL;
+    int withlpc = 0;
+    int linear = 0;
+    int frame_size = FRAME_SIZE;
+    
+    /* note default is running without LPCs (WaveRNN rather than LPCNet) */
     DenoiseState st;
     memset(&st, 0, sizeof(DenoiseState));
+    st.exc_mem = 128;
     
-    short frame[FRAME_SIZE];
-    while (fread(frame, sizeof(short), FRAME_SIZE, fsw) == FRAME_SIZE) {
-      float tmp = (float)rand()/RAND_MAX;
-      float noise_std = 4*tmp*tmp;
-      write_audio(&st, frame, noise_std, fpackedpcm);
+    int o = 0;
+    int opt_idx = 0;
+    while( o != -1 ) {
+        static struct option long_opts[] = {
+            {"lpc", required_argument, 0, 'o'},
+            {"first", no_argument, 0, 'p'},
+            {"linear", no_argument, 0, 'l'},
+            {"frame_size", required_argument, 0, 'f'},
+            {0, 0, 0, 0}
+        };
+        
+	o = getopt_long(argc,argv,"l",long_opts,&opt_idx);
+        
+	switch(o){
+	case 'f':
+	    frame_size = atoi(optarg);
+	    fprintf(stderr, "frame_size: %d\n", frame_size);
+	    break;
+	case 'p':
+	    st.lpc[0] = -0.9;
+	    break;
+	case 'l':
+	    linear = 1;
+	    break;
+	case 'o':
+	    ffeature = fopen(optarg, "rb");
+	    if (ffeature == NULL) {
+		fprintf(stderr, "Can't open %s\n", optarg);
+		exit(1);
+	    }
+	    withlpc = 1;
+	    break;
+	case '?':
+	    goto helpmsg;
+	    break;
+	}
+    }
+    int dx = optind;
+
+    if ((argc - dx) < 2) {
+    helpmsg:
+        fprintf(stderr, "usage: s2packedulaw [--first] [--lpc FeatureFile.f32] Input.s16 Output.pulaw\n");
+        return 0;
+    }
+
+    FILE *fsw = fopen(argv[dx], "rb");
+    if (fsw == NULL) {
+	fprintf(stderr, "Can't open %s\n", argv[1]);
+	exit(1);
+    }
+    
+    FILE *fpackedpcm = fopen(argv[dx+1], "wb");
+    if (fpackedpcm == NULL) {
+	fprintf(stderr, "Can't open %s\n", argv[2]);
+	exit(1);
+    }
+    
+    short frame[frame_size];
+    while (fread(frame, sizeof(short), frame_size, fsw) == (unsigned)frame_size) {
+	if (withlpc) {
+	    float features[NB_FEATURES];
+	    int ret = fread(features, sizeof(float), NB_FEATURES, ffeature);
+	    if (ret != NB_FEATURES) {
+		fprintf(stderr, "feature file ended early!\n");
+		exit(1);		
+	    }
+	    for(int i=0; i<CODEC2_LPC_ORDER; i++) {
+		st.lpc[i] = features[18+i];
+		//fprintf(stderr, "%5.3f ", features[18+i]);
+	    }
+	    //fprintf(stderr, "\n");
+	}
+	if (linear)
+	    write_audio_linear(&st, frame, fpackedpcm, frame_size);
+	else {
+	    write_audio(&st, frame, 0.5, fpackedpcm, frame_size);
+	}
     }
 
     fclose(fsw);

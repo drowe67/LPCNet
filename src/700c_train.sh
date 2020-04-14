2ca3d69892e03b3a14073c03cc61d3c298c2622f
David <david@rowetel.com>
2020-04-15 06:09:19 +0930

    200414c - pretty good, using train_lpcnet.py and 700c_train.sh from dr-700c-dec-200406a

--- src/700c_train.sh
+++ src/700c_train.sh
@@ -13,37 +13,18 @@ fi
 
 train1=all_speechcat_8k
 train2=train_8k
-test1=all_speech_subset_8k
-test2=all_8k
-test3="birch canadian glue oak separately wanted wia peter"
+test1=all_8k
+test2=all_speech_subset_8k
 datestamp=$1
-epochs=10
+epochs=05
 log=${1}.txt
 train=${datestamp}_train
 
-# synth_10ms "c2sim arg for experiment" "experiment label" "path/filename.sw"
-synth_10ms() {
-    path_filename=$3
-    filename=$(basename ${path_filename})
-    c2sim ${path_filename}.sw --rateKWov ${filename}.f32 ${1}
-    test_lpcnet --mag 2 --frame_size 80 --pre 0 ${filename}.f32 ${datestamp}_${filename}_${2}.sw
-}
-
-# synth_40ms "filename"
-synth_40ms() {
-    # no prediction, Codec 2 700C at 40ms frame rate (700 bits/s) from c2dec
-    c2enc 700C ~/Downloads/${1}.sw - --eq --var | c2dec 700C - /dev/null --mlfeat ${1}_dec4.f32
-    test_lpcnet --mag 2 --frame_size 80 --pre 0  ${1}_dec4.f32 ${datestamp}_${1}_40.sw
-}
-    
-synth_test_file() {
-    synth_10ms "${1}" "${2}" ~/Downloads/${test1}
-    synth_10ms "${1}" "${2}" ~/Downloads/${test2}
-    for w in ${test3}
-    do
-	sox ../wav/${w}.wav -r 8000 -t sw ${w}_8k.sw
-	synth_10ms "${1}" "${2}" "${w}_8k"
-    done
+# synth "c2sim arg for experiment" "experiment label" "filename"
+synth() {
+    test=$3
+    c2sim ~/Downloads/${test}.sw --rateKWov ${test}.f32 ${1}
+    test_lpcnet --mag 2 --frame_size 80 --pre 0 ${test}.f32 ${datestamp}_${test}_${2}.sw
 }
 
 # experient "c2sim arg for experiment" "experiment label"
@@ -60,27 +41,29 @@ experiment() {
     cp nnet_data.c src
     make test_lpcnet
 
-    synth_test_file "${1}" "${2}"
+    synth "${1}" "${2}" "${test1}"
+    synth "${1}" "${2}" "${test2}"
 }
 
+rm -f $log
 
-(
-    rm -f $log
-    
-    # assemble some training speech
-    sox -r 8000 -c 1 ~/Downloads/${train1}.sw \
+# assemble some training speech
+sox -r 8000 -c 1 ~/Downloads/${train1}.sw \
     -r 8000 -c 1 ~/Downloads/${train2}.sw \
     -t sw -r 8000 -c 1 ${train}.sw
-    #cp ~/Downloads/${train1}.sw ${train}.sw  
+    
+(
     : '
     experiment "" "none"           # no prediction
 
-    synth_40ms ${test1}    
-    synth_40ms ${test2}
+    # no prediction, Codec 2 700C at 40ms frame rate (700 bits/s) from c2dec
+    c2enc 700C ~/Downloads/${test2}.sw - --eq --var | c2dec 700C - /dev/null --mlfeat ${test2}_dec4.f32
+    test_lpcnet --mag 2 --frame_size 80 --pre 0 ${test2}_dec4.f32 ${datestamp}_${test2}_40.sw
     
     experiment "--first"  "first" # first order predictor
     '
     experiment "--lpc 10" "lpc"   # standard LPC (albiet 10th order)
 
 ) |& tee $log
+
 

0e306165adb247a79f4614336fb3a486cdb069d7
David <david@rowetel.com>
2020-04-27 19:58:02 +0930

    more info for Travis ctest fail

--- unittest/test_core_nn.sh
+++ unittest/test_core_nn.sh
@@ -11,7 +11,7 @@
 
 if [ ! -z $SYNTH ]; then
     ../build_linux/src/dump_data --test --c2pitch --c2voicing ../wav/birch.wav birch.f32
-    diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
+    ../build_linux/src/diff32 birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "synth .f32 OK"
     ../build_linux/src/test_lpcnet -l birch_states.f32 birch.f32 birch_out.raw
     octave -p ../src --no-gui <<< "ret=compare_states('birch_states_targ.f32', 'birch_states.f32'); quit(ret)"
@@ -27,7 +27,7 @@ fi
     
 if [ ! -z $SYNTH_20h ]; then
     ../build_linux/src/dump_data --test --c2pitch --c2voicing ../wav/birch.wav birch.f32
-    diff birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
+    ../build_linux/src/diff32 birch_targ.f32 birch.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "synth .f32 OK"
     ../build_linux/src/test_lpcnet -n lpcnet_20h.f32 -l birch_states.f32 birch.f32 birch_out.raw
     octave -p ../src --no-gui <<< "ret=compare_states('birch_20h_states_targ.f32', 'birch_states.f32'); quit(ret)"
@@ -43,7 +43,7 @@ fi
     
 if [ ! -z $SYNTH_mag ]; then
     ../build_linux/src/dump_data --mag --test --c2pitch --c2voicing ../wav/c01_01.wav c01_01.f32
-    diff c01_01_mag.f32 c01_01.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
+    ../build_linux/src/diff32 c01_01_mag.f32 c01_01.f32 || { echo "ERROR in synth .f32 output! Exiting..."; exit 1; }
     echo "mag .f32 OK"
     ../build_linux/src/test_lpcnet --mag 1 -n lpcnet_190804a.f32 c01_01.f32 c01_01_out.raw
     diff c01_01_190804a_targ.raw c01_01_out.raw || { echo "ERROR in synth .raw output! Exiting..."; exit 1; }
